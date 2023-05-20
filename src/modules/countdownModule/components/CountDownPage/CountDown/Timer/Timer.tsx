65a9d62cd165b01197f75da5efa874aa7940fc4a
Stemmy287 <steem287@gmail.com>
2023-05-20 19:15:08 +0300

    feat: timer

---
+++ src/modules/countdownModule/components/CountDownPage/CountDown/Timer/Timer.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import s from './Timer.module.scss';
+import { useTimer } from 'react-timer-hook';
+
+export const Timer = () => {
+
+	const date = new Date(2023, 4, 31);
+
+	const {
+		days,
+		hours,
+		minutes,
+		seconds
+	} = useTimer({ expiryTimestamp: date });
+
+	const dateForMapping = [
+		{ num: days, title: 'Days' },
+		{ num: hours, title: 'Hours' },
+		{ num: minutes, title: 'Minutes' },
+		{ num: seconds, title: 'Seconds' }
+	];
+
+	return (
+		<div className={s.container}>
+			{dateForMapping.map((el, i) =>
+				<div key={i}>
+					<div className={s.number}>
+						<span>{el.num < 10 ? '0' + el.num : el.num}</span>
+						{i < 3 && <span>:</span>}
+					</div>
+					<div className={s.title}>{el.title}</div>
+				</div>
+			)}
+		</div>
+	);
+};
