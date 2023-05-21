4df0304cac50847c0b1e7c4af760a7aa6b859ea8
Stemmy287 <steem287@gmail.com>
2023-05-21 18:10:04 +0300

    feat: notification

---
+++ src/common/components/Notification/Notification.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import s from './Notification.module.scss';
+import {ReactComponent as Close} from 'common/icons/close.svg';
+import { Button } from '../Button/Button';
+
+type PropsType = {
+	title: string
+	message: string
+	onClose: () => void
+}
+
+export const Notification = ({title, message, onClose}: PropsType) => {
+	return (
+		<div className={s.container}>
+			<Close className={s.close} onClick={onClose}/>
+			<h3>{title}</h3>
+			<span>{message}</span>
+			<Button  callback={onClose}>
+				Close
+			</Button>
+		</div>
+	);
+};
