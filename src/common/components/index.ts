8fbd85b23ea35a514e0c0c87394f71c9d8f7c92d
Stemmy287 <steem287@gmail.com>
2023-05-21 16:55:05 +0300

    feat: popup

--- src/common/components/index.ts
+++ src/common/components/index.ts
@@ -1,3 +1,4 @@
 export * from 'common/components/Footer/Footer'
 export * from 'common/components/Input/Input'
 export * from 'common/components/Button/Button'
+export * from	'common/components/PopUp/PopUp'
