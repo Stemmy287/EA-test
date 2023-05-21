29d0be08f970c418874436a92d7c3097cc89537b
Stemmy287 <steem287@gmail.com>
2023-05-21 16:56:41 +0300

    feat: send email

--- src/common/components/Footer/Footer.tsx
+++ src/common/components/Footer/Footer.tsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from './Footer.module.scss';
 import { Input } from '../Input/Input';
 import {ReactComponent as Arrow} from 'common/icons/arrow-down.svg';
+import { countDownApi } from '../../../modules/countdownModule/countDownApi';
 
 export const Footer = () => {
+
+	const [value, setValue] = useState('')
+	const [error, setError] = useState('')
+
+	const onSubmit = () => {
+
+		const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+		if (value.match(emailFormat)) {
+			countDownApi.sendEmail()
+		} else if (!value) {
+			setError('field is empty')
+		} else {
+			setError('not correct email')
+		}
+
+	}
+
 	return (
 		<footer className={s.container}>
 				<Input
-					value={''}
-					onChange={() => {}}
+					value={value}
+					onChange={(e) => setValue(e.currentTarget.value)}
 					placeholder="Enter your Email and get notified"
-					callback={() => {}}
+					callback={onSubmit}
+					onFocus={() => setError('')}
+					error={error}
 				/>
 			<button className={s.button}>Other Events <Arrow className={s.arrow}/></button>
 		</footer>
